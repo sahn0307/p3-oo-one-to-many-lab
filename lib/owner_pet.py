0973fe0c8e8a7a21792bc118382458a1c70da7f7
Sung-Jin Ahn <sahn0307@gmail.com>
2024-03-17 16:34:54 -0600

    pass tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,27 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Pet must be of type Pet.")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet type")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        type(self).all.append(self)
